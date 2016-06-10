590c194126fd5f380098d009dece1cccd140d7b1
Davide Porzio <salvatore.porzio@postgrad.manchester.ac.uk>
2016-06-10 17:53:28 +0100

    auto

--- Python/NumpyInOut/getFromTxt_Modify_SaveToTxt.py
+++ Python/NumpyInOut/getFromTxt_Modify_SaveToTxt.py
@@ -1,16 +1,16 @@
 import numpy as np
 
 # INPUT
-npDataIn = np.genfromtxt("dataIn.dat",delimiter=" ",names=True,dtype=None)
+dataIn = np.genfromtxt("dataIn.dat",delimiter=" ",names=True,dtype=None)
 
 # MODIFICATION
 # This intermediate step (x,y,z) is unnecessary but it makes it easier in case you would want only the input or the output part
-data = []
-for i in range(len(npDataIn)):
-    x = npDataIn['Col1'][i] + npDataIn['Col1'][i]
-    y = npDataIn['Col2'][i]*2
-    z = npDataIn['Col3'][i]/2
-    data.append([x,y,z])
+dataOut = []
+for i in range(len(dataIn)):
+    x = dataIn['Col1'][i] + dataIn['Col1'][i]
+    y = dataIn['Col2'][i]*2
+    z = dataIn['Col3'][i]/2
+    dataOut.append([x,y,z])
 
 # OUTPUT
-np.savetxt("dataOut.dat",np.array((data)),fmt=['%s','%s','%s'],delimiter=' ',header='Col1 Col2 Col3')
+np.savetxt("dataOut.dat",np.array((dataOut)),fmt=['%s','%s','%s'],delimiter=' ',header='Col1 Col2 Col3')
