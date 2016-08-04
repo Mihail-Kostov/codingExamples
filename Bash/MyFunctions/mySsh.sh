97baf43187213ddc7d3b0e9b4580fe3376804142
Davide Porzio <salvatore.porzio@postgrad.manchester.ac.uk>
2016-08-04 19:25:49 +0100

    auto

--- Bash/MyFunctions/mySsh.sh
+++ Bash/MyFunctions/mySsh.sh
@@ -3,14 +3,16 @@
 # Connect to different ssh addresses
 
 case ${1} in
-  "uboone")       echo "> ssh -X -Y sporzio@uboonegpvm01.fnal.gov"
-                  ssh -X -Y sporzio@uboonegpvm01.fnal.gov ;;
-  "manchester")   echo "> ssh -X -Y davide@higgs.hep.manchester.ac.uk"
-                  ssh -X -Y davide@higgs.hep.manchester.ac.uk ;;
-  "database")     echo "> ssh -X -Y uboonedb@ppdhcp120.hep.manchester.ac.uk"
-                  ssh -X -Y uboonedb@ppdhcp120.hep.manchester.ac.uk ;;
-  "cambridge")    echo "> ssh -X -Y uboone04@pclv.hep.phy.cam.ac.uk"
-                  ssh -X -Y uboone04@pclv.hep.phy.cam.ac.uk ;;
-  *)              echo "> ssh -X -Y davide@${1}.hep.manchester.ac.uk"
-                  ssh -X -Y davide@${1}.hep.manchester.ac.uk ;;
+  "uboone")       	echo "> ssh -X -Y sporzio@uboonegpvm01.fnal.gov"
+                  	ssh -X -Y sporzio@uboonegpvm01.fnal.gov ;;
+  "manchester")   	echo "> ssh -X -Y davide@higgs.hep.manchester.ac.uk"
+                  	ssh -X -Y davide@higgs.hep.manchester.ac.uk ;;
+  "database")     	echo "> ssh -X -Y uboonedb@ppdhcp120.hep.manchester.ac.uk"
+                  	ssh -X -Y uboonedb@ppdhcp120.hep.manchester.ac.uk ;;
+  "database_socket")   echo "> ssh -L 8080:127.0.0.1:5432 uboonedb@ppdhcp120.hep.manchester.ac.uk"
+                  	ssh -L 8080:127.0.0.1:5432 uboonedb@ppdhcp120.hep.manchester.ac.uk ;;
+  "cambridge")    	echo "> ssh -X -Y uboone04@pclv.hep.phy.cam.ac.uk"
+                  	ssh -X -Y uboone04@pclv.hep.phy.cam.ac.uk ;;
+  *)              	echo "> ssh -X -Y davide@${1}.hep.manchester.ac.uk"
+                  	ssh -X -Y davide@${1}.hep.manchester.ac.uk ;;
 esac
--- ROOT/1_Graphs/tgraphdraw.C
+++ ROOT/1_Graphs/tgraphdraw.C
@@ -5,21 +5,22 @@
 #include "TLegend.h"
 #include "TLatex.h"
 
-void draw(){
+void tgraphdraw(){
   const int ndata = 10;
   double xvalue[ndata] = {1,2,3,4,5,6,7,8,9,10};
   double yvalue[ndata] = {6,12,14,20,22,24,35,45,44,53};
   double yerr[ndata] = {5,5,4,7,4,4,5,2,4,4};
 
-  TGraphErrors *graph = new TGraphErrors(ndata,xvalue,yvalue,NULL,yerr);
-  graph->SetTitle("Measurement XYZ; length [cm]; Arb. Units");
+  // TGraphErrors *graph = new TGraphErrors(ndata,xvalue,yvalue,NULL,yerr);
+  TGraph *graph = new TGraph(ndata,xvalue,yvalue); //where x , y are 2 arrays of n points
+  // j->SetTitle("Measurement XYZ; length [cm]; Arb. Units");
 
-  gROOT->SetStyle("Plain");
-  graph->SetMarkerStyle(kOpenCircle);
-  graph->SetMarkerColor(kBlue);
-  graph->SetLineColor(kBlue);
+  // gROOT->SetStyle("Plain");
+  // graph->SetMarkerStyle(kOpenCircle);
+  // graph->SetMarkerColor(kBlue);
+  // graph->SetLineColor(kBlue);
 
   TCanvas *c1 = new TCanvas();
 
-  graph->Draw("APE");
+  graph->Draw("ap");
 }
