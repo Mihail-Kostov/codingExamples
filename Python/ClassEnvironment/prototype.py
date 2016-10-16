9ebb637cfa7718ea26b6279bda3ceddbd7f3c269
Davide Porzio <salvatore.porzio@postgrad.manchester.ac.uk>
2016-10-15 20:12:06 -0500

    small corrections to __main__

--- Python/ClassEnvironment/prototype.py
+++ Python/ClassEnvironment/prototype.py
@@ -1,19 +1,27 @@
 # /usr/bin/python
 
-class Square:
-    'Simple class defining a square'
-    countSquares = 0 # Class variable shared among ALL instances of this class. It is the number of created squares.
+class Figure:
+    'Simple class defining a figure'
+    countFigures = 0 # Class variable shared among ALL instances of this class. It is the number of created figures.
 
-    def __init__(self, name, side): # Class constructor
+    def __init__(self, name): # Class constructor
         self.name = name # Assign name to the instance of the class
-        self.side = side # Assign side
-        Square.countSquares += 1 # Modify the class variable to reflect that a new instance has been created
+        Figure.countFigures += 1 # Modify the class variable to reflect that a new instance has been created
 
     def GetCount(self):
-        return Square.countSquares
+        return Figure.countFigures
+
+class Square(Figure):
+    'Simple subclass defining a square'
+
+    def __init__(self,name,side):
+        Figure.__init__(self,name)
+        self.side = side # Assign side
 
     def GetArea(self):
         return self.side*self.side
+
+
 
 if __name__ == '__main__':
     smallSq = Square('smallSq',5)
