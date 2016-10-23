708ac5c3a3832d0c148ef097105d2ddf82f3e117
Davide Porzio <salvatore.porzio@postgrad.manchester.ac.uk>
2016-10-22 19:46:43 -0500

    mobing stuff around

--- Bash/MyFunctions/mySsh.sh
+++ Bash/MyFunctions/mySsh.sh
@@ -3,8 +3,13 @@
 # Connect to different ssh addresses
 
 case ${1} in
-  "uboone")       	echo "> ssh -X -Y sporzio@uboonegpvm0${2}.fnal.gov"
-                  	ssh -X -Y sporzio@uboonegpvm0${2}.fnal.gov ;;
+  "uboone")       	if [ -z "$2" ]; then
+				echo "> ssh -X -Y sporzio@uboonegpvm03.fnal.gov"
+                  		ssh -X -Y sporzio@uboonegpvm03.fnal.gov
+			else
+				echo "> ssh -X -Y sporzio@uboonegpvm0${2}.fnal.gov"
+                  		ssh -X -Y sporzio@uboonegpvm0${2}.fnal.gov 
+			fi;;
   "manchester")   	echo "> ssh -X -Y davide@higgs.hep.manchester.ac.uk"
                   	ssh -X -Y davide@higgs.hep.manchester.ac.uk ;;
   "database")     	echo "> ssh -X -Y uboonedb@ppdhcp120.hep.manchester.ac.uk"
